bdb25d213bf5f73072faa7f8d1b7e2a11237d209
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-06-15 02:33:32 +0900

    Update:v0.2.0 Improve:refactoring / now send an error msge to stderr Change:main language jp -> en-us Add:logics lib

---
+++ logics/src/lib.rs
@@ -0,0 +1,194 @@
+use std::{io, process};
+
+/// `TempUnit` is a type that represents the unit of temperature.
+pub enum TempUnit {
+    Celsius,
+    Fahrenheit,
+    Kelvin,
+}
+
+/// Sends a error when when an Error of Failed to Get occurs.
+fn failed_to_get(err: io::Error) {
+    eprintln!("Failed to get: {}", err);
+}
+
+/// Asks and gets a base temperature unit.
+///
+/// Returns the `Result<TempUnit, ()>`
+pub fn get_temp_unit() -> Result<TempUnit, ()> {
+    println!("Select the base temperature unit(a, b, c)");
+    println!("a.Celsius(°C)");
+    println!("b.Fahrenheit(°F)");
+    println!("c.Absolute(Kelvin)(K)");
+
+    let mut base_temp_unit = String::new();
+    io::stdin()
+        .read_line(&mut base_temp_unit)
+        .unwrap_or_else(|err| {
+            failed_to_get(err);
+            process::exit(1);
+        });
+
+    // It's a string, so convert it to a string slice.
+    // And remove leading and trailing whitespace.
+    let base_temp_unit: &str = &base_temp_unit.trim();
+
+    match base_temp_unit {
+        "a" => Ok(TempUnit::Celsius),
+        "b" => Ok(TempUnit::Fahrenheit),
+        "c" => Ok(TempUnit::Kelvin),
+        _ => {
+            eprintln!("[Invalid value]");
+            eprintln!("Please enter `a` or `b` or `c`");
+            eprintln!("========================================");
+            Err(())
+        }
+    }
+}
+
+/// Asks and gets a base temperature value.
+///
+/// Returns the `Result<f64, ()>`.
+pub fn get_temp_val() -> Result<f64, ()> {
+    println!("Enter the temperature value");
+
+    let mut temp_value = String::new();
+    io::stdin()
+        .read_line(&mut temp_value)
+        .unwrap_or_else(|err| {
+            failed_to_get(err);
+            process::exit(1);
+        });
+
+    // It's a string, so convert it to a f64.
+    let temp_value: f64 = match temp_value.trim().parse() {
+        Ok(num) => num,
+        _ => {
+            eprintln!("Please enter the numerical value:(");
+            return Err(());
+        }
+    };
+    Ok(temp_value)
+}
+
+/// Holds three different temperature units.
+pub struct Temps {
+    pub celsius: f64,
+    pub fahrenheit: f64,
+    pub kelvin: f64,
+}
+
+impl Temps {
+    /// Creates a new "Temps" based on the given three temperatures.
+    pub fn new(celsius: f64, fahrenheit: f64, kelvin: f64) -> Self {
+        Self {
+            celsius,
+            fahrenheit,
+            kelvin,
+        }
+    }
+
+    /// Sends the result.
+    pub fn show_result(self) {
+        println!(
+            "{}",
+            if self.kelvin < 0.0 {
+                "*Temperature is below absolute zero."
+            } else {
+                "[Converted successfully]"
+            }
+        );
+        println!("Celsius: {}°C", self.celsius);
+        println!("Fahrenheit: {}°F", self.fahrenheit);
+        println!("Absolute(Kelvin): {}K", self.kelvin);
+    }
+}
+
+/// Calculates temperatures based on the given base temperatures and unit.
+///
+/// And returns the Result<Temps, ()>
+/// 
+/// #Examples:
+/// 
+/// ```
+/// let temps = calc_temp(TempUnit::Celsius, 36.5).unwrap();
+/// let result = format!("\
+/// Celsius: {}
+/// Fahrenheit: {}
+/// Absolute(Kelvin): {}",
+///     temps.celsius,
+///     temps.fahrenheit,
+///     temps.kelvin
+/// );
+/// assert_eq!(
+///     result,
+///     "\
+/// Celsius: 36.5
+/// Fahrenheit: 97.7
+/// Absolute(Kelvin): 309.65"
+/// ```
+pub fn calc_temp(temp_unit: TempUnit, temp_value: f64) -> Result<Temps, ()> {
+    println!("Calculating...");
+
+    let abs0: f64 = 273.15; // absolute zero in celsius temp.
+    let c_temp = match temp_unit {
+        TempUnit::Celsius => temp_value, // temp_value is already Celsius temp.
+        TempUnit::Fahrenheit => (temp_value - 32.0) / 1.8, // °F -> °C
+        TempUnit::Kelvin => temp_value - abs0, // K -> °C
+    };
+    let f_temp = match temp_unit {
+        TempUnit::Celsius => temp_value * 1.8 + 32.0, // °C -> °F
+        TempUnit::Fahrenheit => temp_value,           // temp_value is already Fahrenheit temp.
+        TempUnit::Kelvin => (temp_value - abs0) * 1.8 + 32.0, // K -> °F
+    };
+    let k_temp = match temp_unit {
+        TempUnit::Celsius => temp_value + abs0, // °C -> K
+        TempUnit::Fahrenheit => (temp_value - 32.0) / 1.8 + abs0, // °F -> K
+        TempUnit::Kelvin => temp_value,         // temp_value is already Absolute(Kelvin) temp.
+    };
+
+    Ok(Temps::new(c_temp, f_temp, k_temp))
+}
+
+/// Asks the user if they wish to continue with the conversion.
+///
+/// Returns a bool.
+pub fn ask_wanna_continue() -> bool {
+    println!("\nWould you like to continue with the conversion?[y, n]");
+    let mut will_continue = String::new();
+    io::stdin()
+        .read_line(&mut will_continue)
+        .unwrap_or_else(|err| {
+            failed_to_get(err);
+            process::exit(1);
+        });
+    match will_continue.trim() {
+        "y" => true,
+        _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successfull_temp_calc() {
+        let temps = calc_temp(TempUnit::Celsius, 36.5).unwrap();
+        let result = format!("\
+Celsius: {}
+Fahrenheit: {}
+Absolute(Kelvin): {}",
+            temps.celsius,
+            temps.fahrenheit,
+            temps.kelvin
+        );
+        assert_eq!(
+            result,
+            "\
+Celsius: 36.5
+Fahrenheit: 97.7
+Absolute(Kelvin): 309.65"
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,28 @@
-// Modules
-use std::io; // 入出力
-
-struct Temp {
-    c: &'static str,
-    f: &'static str,
-    k: &'static str,
-}
-
-// DB
-const LINE: &str = "========================================";
-const TEMP: Temp = Temp {
-    c: "摂氏温度",
-    f: "華氏温度",
-    k: "絶対温度",
-};
-const ERR_FTG: &str = "取得に失敗しました。";
+use logics::*;
 
 fn main() {
     loop {
-        println!("基準の温度単位を選んで下さい(a, b, c)");
-        println!("a.{}(°C)", TEMP.c);
-        println!("b.{}(°F)", TEMP.f);
-        println!("c.{}(K)", TEMP.k);
-        let mut input_temp_unit = String::new();
-        io::stdin().read_line(&mut input_temp_unit).expect(ERR_FTG);
-        let input_temp_unit: &str = &input_temp_unit.trim(); // String型なので &strに変換
+        let base_temp_unit = match get_temp_unit() {
+            Ok(u) => u,
+            Err(_) => continue,
+        };
 
-        match input_temp_unit {
-            "a" | "b" | "c" => {
-                println!("温度を入力して下さい。");
-                let mut input_temp_value = String::new();
-                io::stdin().read_line(&mut input_temp_value).expect(ERR_FTG);
-                let input_temp_value: f64 = match input_temp_value.trim().parse() {
-                    Ok(num) => num,
-                    _ => {
-                        println!("数値を入力して下さい");
-                        continue;
-                    }
-                };
+        let base_temp_val = match get_temp_val() {
+            Ok(v) => v,
+            Err(_) => continue,
+        };
 
-                println!("計算中...");
-                const ABS0: f64 = 273.15; // 絶対零度の絶対値
-                let c_temp = match input_temp_unit {
-                    "a" => input_temp_value,                // input_temp_value が既に摂氏温度
-                    "b" => (input_temp_value - 32.0) / 1.8, // 華氏 -> 摂氏
-                    "c" => input_temp_value - ABS0,         // ケルビン -> 摂氏
-                    _ => {
-                        err_msg();
-                        continue;
-                    }
-                };
-                let f_temp = match input_temp_unit {
-                    "a" => input_temp_value * 1.8 + 32.0,          // 摂氏 -> 華氏
-                    "b" => input_temp_value,                       // 華氏温度そのもの
-                    "c" => (input_temp_value - ABS0) * 1.8 + 32.0, // ケルビン -> 華氏
-                    _ => {
-                        err_msg();
-                        continue;
-                    }
-                };
-                let k_temp = match input_temp_unit {
-                    "a" => input_temp_value + ABS0,                // 摂氏 -> ケルビン
-                    "b" => (input_temp_value - 32.0) / 1.8 + ABS0, // 華氏 -> ケルビン
-                    "c" => input_temp_value,                       // 絶対温度そのもの
-                    _ => {
-                        err_msg();
-                        continue;
-                    }
-                };
+        let temps = match calc_temp(base_temp_unit, base_temp_val) {
+            Ok(tmps) => tmps,
+            Err(_) => continue,
+        };
 
-                let wheth_too_small = if k_temp < 0.0 {
-                    let msg = "※温度が絶対温度を下回っています。";
-                    msg
-                } else {
-                    let msg = "[正常に換算されました]";
-                    msg
-                };
-                println!("{}", wheth_too_small);
-                println!("{} : {}°C", TEMP.c, c_temp);
-                println!("{} : {}°F", TEMP.f, f_temp);
-                println!("{} : {}K", TEMP.k, k_temp);
+        temps.show_result();
 
-                println!("\n換算を続けますか?[y, n]");
-                let mut wheth_continue = String::new();
-                io::stdin()
-                    .read_line(&mut wheth_continue)
-                    .expect("取得に失敗しました。");
-
-                match wheth_continue.trim() {
-                    "y" => continue,
-                    _ => break,
-                };
-            }
-            _ => {
-                println!("[無効な値]");
-                println!("a または b または c を入力してください。");
-                println!("{}", LINE);
-                continue;
-            }
+        if ask_wanna_continue() {
+            continue;
+        } else {
+            break;
         }
     }
-}
-
-fn err_msg() {
-    println!("計算に失敗しました\n{}", LINE);
 }
